c6d6dd3f1755baf3fa6b14bf5e727ad6ae87e713
harnaman-hk <harnaman24@gmail.com>
2020-06-29 00:28:42 +0530

    changes

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 # the main script will will be run by RPi
 import deliveryMotors
 import rotating
+import unloading
 
 if __name__ == "__main__":
     
+    unloading.forward()
+
     # scan package for pincode 
     pincode = rotating.get_pincode()
 
     # deliver to cart associated with the pincode 
     deliveryMotors.deliver(pincode)
+
+
